155cc5f89154ea2d8c18d6d897aafec1a84df5d3
Olly King <ollyking123@gmail.com>
2025-03-25 14:50:20 +0000

    add some domain model and basic repository interfaces

---
+++ internal/datasources/user.go
@@ -0,0 +1,19 @@
+package datasources
+
+import (
+	"context"
+	"github.com/oking02/surfe-challenge/internal/domain"
+)
+
+type UserRepository interface {
+	UserLister
+	UserFetcher
+}
+
+type UserLister interface {
+	ListUsers(context.Context, domain.ClientID) ([]domain.User, error)
+}
+
+type UserFetcher interface {
+	GetUser(context.Context, domain.ClientID, domain.UserID) (domain.User, error)
+}
---
+++ internal/domain/action.go
@@ -0,0 +1,33 @@
+package domain
+
+import "time"
+
+type ActionID int64
+
+type ActionType string
+
+const (
+	ADD_CONTACT   ActionType = "ADD_CONTACT"
+	CONNECT_CRM   ActionType = "CONNECT_CRM"
+	EDIT_CONTACT  ActionType = "EDIT_CONTACT"
+	VIEW_CONTACTS ActionType = "VIEW_CONTACTS"
+	REFER_USER    ActionType = "REFER_USER"
+	WELCOME       ActionType = "WELCOME"
+)
+
+type Action struct {
+	// ID is the unique identifier for this action
+	ID ActionID
+	// Type is a enum value of
+	Type ActionType
+	// UserID refers to the user who took this action
+	UserID UserID
+	// TargetID is only for some action types
+	// is refers to what user this action is performed on
+	TargetID UserID
+	// CreatedAt refers when this action was performed
+	CreatedAt time.Time
+	// ClientID is the identifier for the client
+	// the parent user is associated with
+	ClientID ClientID
+}
